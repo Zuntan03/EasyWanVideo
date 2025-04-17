36582bbf2a823c6a81d60c0160b124c226cb7629
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2025-04-17 19:53:27 +0900

    実験的に FramePack 対応を入れてみました。 FramePack.bat で長時間のインストール後に起動します。 50GB 程度のストレージが必要です。

---
+++ EasyWanVideo/SetupFramePack.bat
@@ -0,0 +1,72 @@
+@echo off
+chcp 65001 > NUL
+set EASY_TOOLS=%~dp0..\EasyTools
+set GITHUB_CLONE_OR_PULL_HASH=%EASY_TOOLS%\Git\GitHub_CloneOrPull_Hash.bat
+set PYTHON_ACTIVATE=%EASY_TOOLS%\Python\Python_Activate.bat
+set EMBEDDABLE_PYTHON=%EASY_TOOLS%\Python\env\python310
+
+pushd %~dp0..
+
+@REM 2025/04/17 0e5fe5d7ca13c76fb8e13708f4b92e7c7a34f20c
+call %GITHUB_CLONE_OR_PULL_HASH% lllyasviel FramePack main 0e5fe5d7ca13c76fb8e13708f4b92e7c7a34f20c
+if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
+
+popd rem %~dp0..
+pushd %~dp0..\FramePack
+
+call %PYTHON_ACTIVATE%
+if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
+
+echo python -m pip install -qq -U pip setuptools wheel
+python -m pip install -qq -U pip setuptools wheel
+if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
+
+@REM https://github.com/woct0rdho/SageAttention/releases
+@REM https://github.com/woct0rdho/triton-windows/releases
+echo pip install -qq --pre torch==2.7.0.dev20250311+cu128 torchvision==0.22.0.dev20250312+cu128 torchaudio==2.6.0.dev20250312+cu128 --index-url https://download.pytorch.org/whl/nightly/cu128
+pip install -qq --pre torch==2.7.0.dev20250311+cu128 torchvision==0.22.0.dev20250312+cu128 torchaudio==2.6.0.dev20250312+cu128 --index-url https://download.pytorch.org/whl/nightly/cu128
+if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
+
+@REM triton
+echo pip install -qq --pre triton-windows==3.3.0a0.post14
+pip install -qq --pre triton-windows==3.3.0a0.post14
+if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
+
+set "TRITON_CACHE=C:\Users\%USERNAME%\.triton\cache"
+set "TORCH_INDUCTOR_TEMP=C:\Users\%USERNAME%\AppData\Local\Temp\torchinductor_%USERNAME%"
+
+if not exist "%TRITON_CACHE%" ( goto :EASY_TRITON_CACHE_NOT_FOUND )
+echo rmdir /S /Q "%TRITON_CACHE%"
+rmdir /S /Q "%TRITON_CACHE%"
+if %ERRORLEVEL% neq 0 ( pause & exit /b 1 )
+:EASY_TRITON_CACHE_NOT_FOUND
+
+if not exist "%TORCH_INDUCTOR_TEMP%" ( goto :EASY_TORCH_INDUCTOR_TEMP_NOT_FOUND )
+echo rmdir /S /Q "%TORCH_INDUCTOR_TEMP%"
+rmdir /S /Q "%TORCH_INDUCTOR_TEMP%"
+if %ERRORLEVEL% neq 0 ( pause & exit /b 1 )
+:EASY_TORCH_INDUCTOR_TEMP_NOT_FOUND
+
+@REM sageattention
+echo pip install -qq https://github.com/woct0rdho/SageAttention/releases/download/v2.1.1-windows/sageattention-2.1.1+cu128torch2.7.0-cp310-cp310-win_amd64.whl
+pip install -qq https://github.com/woct0rdho/SageAttention/releases/download/v2.1.1-windows/sageattention-2.1.1+cu128torch2.7.0-cp310-cp310-win_amd64.whl
+if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
+
+@REM requirements.txt
+echo pip install -qq -r requirements.txt
+pip install -qq -r requirements.txt
+if %ERRORLEVEL% neq 0 ( pause & popd & exit /b 1 )
+
+@REM compile
+if exist %EMBEDDABLE_PYTHON%\ (
+	@REM tcc.exe & VS Build Tools cl.exe
+	if not exist venv\Scripts\Include\Python.h (
+		echo xcopy /SQY %EMBEDDABLE_PYTHON%\include\*.* venv\Scripts\Include\
+		xcopy /SQY %EMBEDDABLE_PYTHON%\include\*.* venv\Scripts\Include\
+
+		echo xcopy /SQY %EMBEDDABLE_PYTHON%\libs\*.* venv\Scripts\libs\
+		xcopy /SQY %EMBEDDABLE_PYTHON%\libs\*.* venv\Scripts\libs\
+	)
+)
+
+popd rem %~dp0..\FramePack
---
+++ FramePack.bat
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001 > NUL
+
+echo http://localhost:7840
+start http://localhost:7840
+
+call %~dp0FramePack_NoArgs.bat --port 7840 %*
+if %ERRORLEVEL% neq 0 ( exit /b 1 )
+
+cmd /k
